7a208ca32d17a24f046bb12ce3f7254fde8d4dcd
Oleksandr Onsha <Oleksandr_Onsha@epam.com>
2018-11-02 13:11:30 +0200

    update version to 0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='syndicate',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
