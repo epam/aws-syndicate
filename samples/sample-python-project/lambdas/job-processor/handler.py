4188c4da31517775bd4603f95d32e100bbb87503
onsha <Oleksandr_Onsha@epam.com>
2020-02-10 14:25:22 +0200

    [sample] fix project path, add creation time and last upd time

--- samples/sample-python-project/lambdas/job-processor/handler.py
+++ samples/sample-python-project/lambdas/job-processor/handler.py
@@ -1,4 +1,11 @@
+import time
+from datetime import datetime
+
 from models.Job import Job
+
+
+def current_timestamp():
+    return int(datetime.timestamp(datetime.now()))
 
 
 def lambda_handler(event, context):
@@ -7,13 +14,16 @@ def lambda_handler(event, context):
     for record in event['Records']:
         # registering job
         job_id = record['messageId']
-        new_job = Job(job_id=job_id, status='CREATED')
+        new_job = Job(job_id=job_id, status='CREATED',
+                      creation_date=current_timestamp())
         new_job.save()
 
         #  job processing code here
+        time.sleep(2)
 
         # update job status
         new_job.status = 'DONE'
+        new_job.last_updated_data_sec = current_timestamp()
         new_job.save()
         processed_jobs.append(job_id)
     return 'Jobs processed: {}'.format(processed_jobs)
