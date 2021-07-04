31656504d705f44cfda18b46fc6ef72d32369717
Vladyslav_Yakymchuk <Vladysalv_Yakymchuk@epam.com>
2021-07-04 16:43:52 +0300

    Implementation of transformer for api gateway

--- syndicate/core/transform/build_meta_transformer.py
+++ syndicate/core/transform/build_meta_transformer.py
@@ -16,20 +16,28 @@
 from abc import abstractmethod
 
 from syndicate.core.build.meta_processor import resolve_meta
-from syndicate.core.constants import IAM_POLICY, IAM_ROLE, LAMBDA_TYPE
+from syndicate.core.constants import IAM_POLICY, IAM_ROLE, LAMBDA_TYPE, \
+    DYNAMO_TABLE_TYPE, S3_BUCKET_TYPE, CLOUD_WATCH_RULE_TYPE, SQS_QUEUE_TYPE, \
+    API_GATEWAY_TYPE, SNS_TOPIC_TYPE, CLOUD_WATCH_ALARM_TYPE, \
+    EC2_INSTANCE_TYPE
 
 
-class BuildMetaTransformer(object):
+class BuildMetaTransformer:
 
     def __init__(self):
-        from syndicate.core import CONFIG, RESOURCES_PROVIDER
-        self.config = CONFIG
-        self.resources_provider = RESOURCES_PROVIDER
-
+        self.resources = list()
         self.transformer_mapping = {
             IAM_POLICY: self._transform_iam_managed_policy,
             IAM_ROLE: self._transform_iam_role,
-            LAMBDA_TYPE: self._transform_lambda
+            LAMBDA_TYPE: self._transform_lambda,
+            DYNAMO_TABLE_TYPE: self._transform_dynamo_db_table,
+            S3_BUCKET_TYPE: self._transform_s3_bucket,
+            CLOUD_WATCH_RULE_TYPE: self._transform_cloud_watch_rule,
+            SQS_QUEUE_TYPE: self._transform_sqs_queue,
+            API_GATEWAY_TYPE: self._transform_api_gateway,
+            SNS_TOPIC_TYPE: self._transform_sns_topic,
+            CLOUD_WATCH_ALARM_TYPE: self._transform_cloudwatch_alarm,
+            EC2_INSTANCE_TYPE: self._transform_ec2_instance,
         }
 
     def transform_build_meta(self, build_meta):
@@ -38,10 +46,13 @@ class BuildMetaTransformer(object):
             resource_type = resource.get('resource_type')
             transformer = self.transformer_mapping.get(resource_type)
             if transformer is None:
-                raise ValueError(
-                    "Transformation is not supported for resources "
-                    "of the '{}' type".format(resource_type))
-            transformer(name=name, resource=resource)
+                continue
+            transformed_resource = transformer(name=name, resource=resource)
+            if isinstance(transformed_resource, list):
+                for res in transformed_resource:
+                    self.resources.append(res)
+            else:
+                self.resources.append(transformed_resource)
         return self._compose_template()
 
     @abstractmethod
@@ -58,6 +69,42 @@ class BuildMetaTransformer(object):
 
     @abstractmethod
     def _transform_lambda(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_dynamo_db_table(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_s3_bucket(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_cloud_watch_rule(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_api_gateway(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_sns_topic(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_cloudwatch_alarm(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_ec2_instance(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_sqs_queue(self, name, resource):
+        pass
+
+    @abstractmethod
+    def _transform_dynamodb_stream(self, name, resource):
         pass
 
     @abstractmethod
