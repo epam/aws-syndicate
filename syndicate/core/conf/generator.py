489f8d885823a42dba0684fd459c0991b7fe8082
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2021-10-21 18:31:24 +0300

    Attempt to load creds if they're not passed while generating config

--- syndicate/core/conf/generator.py
+++ syndicate/core/conf/generator.py
@@ -18,6 +18,7 @@ import sys
 
 import yaml
 from botocore.exceptions import ClientError
+from boto3.session import Session
 
 from syndicate.commons.log_helper import get_logger, get_user_logger
 from syndicate.connection.sts_connection import STSConnection
@@ -42,6 +43,16 @@ def generate_configuration_files(name, config_path, region,
                                  access_key, secret_key,
                                  bundle_bucket_name, prefix, suffix,
                                  project_path=None):
+    if not access_key or not secret_key:
+        _USER_LOG.warn("Access_key or secret_key weren't passed. "
+                  "Attempting to load them")
+        credentials = Session().get_credentials()
+        if not credentials:
+            raise AssertionError("No credentials could be found")
+        current_credentials = credentials.get_frozen_credentials()
+        access_key = current_credentials.access_key
+        secret_key = current_credentials.secret_key
+
     try:
         sts = STSConnection(region=region,
                             aws_access_key_id=access_key,
