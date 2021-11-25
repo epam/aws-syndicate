beeeadaa1bacd8461ad27b52183bd7abc6596246
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2021-11-25 13:11:03 +0200

    Correct logs a bit

--- syndicate/core/build/meta_processor.py
+++ syndicate/core/build/meta_processor.py
@@ -144,17 +144,13 @@ def _check_duplicated_resources(initial_meta_dict, additional_item_name,
 
             init_compression = initial_item.get("minimum_compression_size")
             if init_compression:
+                _LOG.warn(f"Found 'minimum_compression_size': "
+                          f"{init_compression} inside root "
+                          f"deployment_resources. The value "
+                          f"'{additional_item.get('minimum_compression_size')}'"
+                          f" from: {additional_item} will be overwritten")
                 additional_item['minimum_compression_size'] = init_compression
-                _LOG.warn("Found 'minimum_compression_size' inside root "
-                          "deployment_resources.json. Its "
-                          f"value: {init_compression} is remaining the same")
-            else:
-                additional_comp_size = additional_item. \
-                    get('minimum_compression_size')
-                _LOG.warn("Not found 'minimum_compression_size' inside root "
-                          "deployment_resources.json. "
-                          f"{f'Setting: {additional_comp_size} as root from' if additional_comp_size else f'Skipping sub-item'}: "
-                          f"{additional_item_name} {additional_item}")
+
             additional_item = _merge_api_gw_list_typed_configurations(
                 initial_item,
                 additional_item,
