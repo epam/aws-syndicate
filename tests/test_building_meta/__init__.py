5fd2a29b20efa58edfd979e2c374b17e4efdbf86
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2021-12-03 16:45:07 +0200

    Refactoring

--- tests/test_building_meta/__init__.py
+++ tests/test_building_meta/__init__.py
@@ -1,14 +1,18 @@
+import json
 import os
 import shutil
-import json
 import tempfile
 import unittest
 from pathlib import Path
 
 from syndicate.core.build.meta_processor import _look_for_configs
+from syndicate.core.constants import RESOURCES_FILE_NAME
 
 
 class TestBuildingMeta(unittest.TestCase):
+    MAIN_D_R = None
+    SUB_D_R = None
+
     @classmethod
     def setUpClass(cls) -> None:
         cls.testing_sub_dir = 'syndicate_tests'
@@ -17,6 +21,11 @@ class TestBuildingMeta(unittest.TestCase):
 
     def setUp(self) -> None:
         self.bundle_name = 'bundle_name'
+
+        if not self.MAIN_D_R or \
+                not self.SUB_D_R:
+            raise AssertionError("MAIN_DEPLOYMENT_RESOURCES and "
+                                 "SUB_DEPLOYMENT_RESOURCES must be specified")
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -35,3 +44,8 @@ class TestBuildingMeta(unittest.TestCase):
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'w') as file:
             json.dump(data, file)
+
+    def write_main_and_sub_deployment_resources(self):
+        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.MAIN_D_R)
+        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
+                               self.SUB_D_R)
--- tests/test_building_meta/tests_building_meta.py
+++ tests/test_building_meta/tests_building_meta.py
@@ -1,34 +1,21 @@
-import json
-import os
-from pathlib import Path
 from copy import deepcopy
 
-from syndicate.core.constants import RESOURCES_FILE_NAME
 from tests.test_building_meta import TestBuildingMeta
 
 
 class TestApiGateway(TestBuildingMeta):
-    def setUp(self) -> None:
-        super().setUp()
-        self.resource_name = 'test_api'
-        self.main_d_r = {
-            self.resource_name: {
-                "dependencies": [],
-                "resource_type": "api_gateway",
-                "deploy_stage": "test",
-                "authorizers": {},
-                "resources": {}
-            }
+    RESOURCE_NAME = 'test_api'
+    MAIN_D_R = {
+        RESOURCE_NAME: {
+            "dependencies": [],
+            "resource_type": "api_gateway",
+            "deploy_stage": "test",
+            "authorizers": {},
+            "resources": {}
         }
-        self.sub_d_r = {
-            self.resource_name: {
-                "dependencies": [],
-                "resource_type": "api_gateway",
-                "deploy_stage": "test",
-                "authorizers": {},
-                "resources": {}
-            }
-        }
+    }
+    SUB_D_R = deepcopy(MAIN_D_R)
+
 
 class TestApiGatewayDuplicatedResources(TestApiGateway):
     def setUp(self) -> None:
@@ -39,44 +26,42 @@ class TestApiGatewayDuplicatedResources(TestApiGateway):
                 "GET": {}
             }
         }
-        self.main_d_r[self.resource_name]['resources'] = self.resources
-        self.sub_d_r[self.resource_name]['resources'] = self.resources
+        self.MAIN_D_R[self.RESOURCE_NAME]['resources'] = \
+            self.resources
+        self.SUB_D_R[self.RESOURCE_NAME]['resources'] = \
+            self.resources
 
     def test_duplicated_resources(self):
-        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.main_d_r)
-        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
-                               self.sub_d_r)
+        self.write_main_and_sub_deployment_resources()
 
         with self.assertRaises(AssertionError) as context:
             self.dispatch(resources_meta={})
         self.assertEqual(str(context.exception),
                          "API '{0}' has duplicated resource '{1}'! Please, "
                          "change name of one resource or remove one.".format(
-                             self.resource_name, '/signup')
+                             self.RESOURCE_NAME, '/signup')
                          )
+
 
 class TestApiGatewayCompressionSize(TestApiGateway):
     def setUp(self) -> None:
         super().setUp()
-        self.main_d_r[self.resource_name]['minimum_compression_size'] = 400
-        self.sub_d_r[self.resource_name]['minimum_compression_size'] = 300
+        self.MAIN_D_R[self.RESOURCE_NAME]['minimum_compression_size'] = 400
+        self.SUB_D_R[self.RESOURCE_NAME]['minimum_compression_size'] = 300
+
     def test_resolving_compression_size_main(self):
         # compression size must be taken
         # rightly from MAIN deployment_resouces.json
-        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.main_d_r)
-        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
-                               self.sub_d_r)
+        self.write_main_and_sub_deployment_resources()
         resources_meta = {}
         self.dispatch(resources_meta)
         self.assertEqual(
             resources_meta['test_api']['minimum_compression_size'], 400)
 
     def test_resolving_compression_size_main_does_not_exist(self):
-        self.main_d_r["test_api"].pop('minimum_compression_size')
+        self.MAIN_D_R[self.RESOURCE_NAME].pop('minimum_compression_size')
 
-        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.main_d_r)
-        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
-                               self.sub_d_r)
+        self.write_main_and_sub_deployment_resources()
 
         resource_meta = {}
         self.dispatch(resource_meta)
@@ -84,12 +69,10 @@ class TestApiGatewayCompressionSize(TestApiGateway):
             resource_meta['test_api']['minimum_compression_size'], 300)
 
     def test_resolving_compression_size_does_not_exist(self):
-        self.main_d_r['test_api'].pop('minimum_compression_size')
-        self.sub_d_r['test_api'].pop('minimum_compression_size')
+        self.MAIN_D_R[self.RESOURCE_NAME].pop('minimum_compression_size')
+        self.SUB_D_R[self.RESOURCE_NAME].pop('minimum_compression_size')
 
-        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.main_d_r)
-        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
-                               self.sub_d_r)
+        self.write_main_and_sub_deployment_resources()
         resource_meta = {}
         self.dispatch(resource_meta)
         self.assertNotIn('minimum_compression_size',
@@ -98,34 +81,28 @@ class TestApiGatewayCompressionSize(TestApiGateway):
 
 class TestEqualResourcesFound(TestBuildingMeta):
     """It applies to all the resouces types except API_GATEWAY"""
-    def setUp(self) -> None:
-        super().setUp()
-        self.resouce_name = 'test_bucket'
-        self.resouce_1 = {
-            self.resouce_name: {
+    RESOURCE_NAME = 'test_bucket'
+    MAIN_D_R = {
+            RESOURCE_NAME: {
                 "resource_type": "s3_bucket",
                 "cors": []
             }
         }
-        self.resouce_2 = deepcopy(self.resouce_1)
+    SUB_D_R = deepcopy(MAIN_D_R)
 
     def test_two_equal_resouces_found(self):
-        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.resouce_1)
-        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
-                               self.resouce_2)
+        self.write_main_and_sub_deployment_resources()
 
         with self.assertRaises(AssertionError) as context:
             self.dispatch(resources_meta={})
         self.assertEqual(str(context.exception),
                          'Warn. Two equals resources descriptions were found! '
                          'Please, remove one of them. Resource name:'
-                         ' {0}'.format(self.resouce_name))
+                         ' {0}'.format(self.RESOURCE_NAME))
 
     def test_two_resouces_with_equal_names_found(self):
-        self.resouce_2[self.resouce_name]['location'] = 'eu-west-1'
-        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.resouce_1)
-        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
-                               self.resouce_2)
+        self.SUB_D_R[self.RESOURCE_NAME]['location'] = 'eu-west-1'
+        self.write_main_and_sub_deployment_resources()
 
         with self.assertRaises(AssertionError) as context:
             self.dispatch(resources_meta={})
@@ -133,6 +110,6 @@ class TestEqualResourcesFound(TestBuildingMeta):
                          "Error! Two resources with equal names were found! "
                          "Name: {0}. Please, rename one of them. First "
                          "resource: {1}. Second resource: {2}".format(
-                             self.resouce_name,
-                             self.resouce_1[self.resouce_name],
-                             self.resouce_2[self.resouce_name]))
+                             self.RESOURCE_NAME,
+                             self.MAIN_D_R[self.RESOURCE_NAME],
+                             self.SUB_D_R[self.RESOURCE_NAME]))
