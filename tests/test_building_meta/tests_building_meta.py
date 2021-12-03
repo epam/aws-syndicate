b0db46a6322d9bd20fd6cadce6f238436f2f285d
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2021-12-03 16:12:02 +0200

    Add tests which check equal resouces definitions

--- tests/test_building_meta/tests_building_meta.py
+++ tests/test_building_meta/tests_building_meta.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pathlib import Path
+from copy import deepcopy
 
 from syndicate.core.constants import RESOURCES_FILE_NAME
 from tests.test_building_meta import TestBuildingMeta
@@ -66,3 +67,46 @@ class TestApiGatewayCompressionSize(TestBuildingMeta):
         self.dispatch(resource_meta)
         self.assertNotIn('minimum_compression_size',
                          resource_meta['test_api'])
+
+
+class TestEqualResourcesFound(TestBuildingMeta):
+    """It applies to all the resouces types except API_GATEWAY"""
+    def setUp(self) -> None:
+        super().setUp()
+        self.resouce_name = 'test_bucket'
+        self.resouce_1 = {
+            self.resouce_name: {
+                "resource_type": "s3_bucket",
+                "cors": []
+            }
+        }
+        self.resouce_2 = deepcopy(self.resouce_1)
+
+    def test_two_equal_resouces_found(self):
+        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.resouce_1)
+        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
+                               self.resouce_2)
+
+        with self.assertRaises(AssertionError) as context:
+            self.dispatch(resources_meta={})
+        self.assertEqual(str(context.exception),
+                         'Warn. Two equals resources descriptions were found! '
+                         'Please, remove one of them. Resource name:'
+                         ' {0}'.format(self.resouce_name))
+
+    def test_two_resouces_with_equal_names_found(self):
+        self.resouce_2[self.resouce_name]['location'] = 'eu-west-1'
+        self.write_json_to_tmp(RESOURCES_FILE_NAME, self.resouce_1)
+        self.write_json_to_tmp(Path('sub_path', RESOURCES_FILE_NAME),
+                               self.resouce_2)
+
+        with self.assertRaises(AssertionError) as context:
+            self.dispatch(resources_meta={})
+        self.assertEqual(str(context.exception),
+                         "Error! Two resources with equal names were found! "
+                         "Name: {0}. Please, rename one of them. First "
+                         "resource: {1}. Second resource: {2}".format(
+                             self.resouce_name,
+                             self.resouce_1[self.resouce_name],
+                             self.resouce_2[self.resouce_name]))
+
