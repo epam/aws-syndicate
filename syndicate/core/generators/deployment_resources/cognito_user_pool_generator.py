bda2a48911eac6bd05f9f2c6523a42a65a2e613c
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2021-12-21 17:03:42 +0200

    Correct comment

--- syndicate/core/generators/deployment_resources/cognito_user_pool_generator.py
+++ syndicate/core/generators/deployment_resources/cognito_user_pool_generator.py
@@ -6,7 +6,7 @@ from syndicate.core.constants import COGNITO_USER_POOL_TYPE
 class CognitoUserPoolGenerator(BaseDeploymentResourceGenerator):
     RESOURCE_TYPE = COGNITO_USER_POOL_TYPE
     CONFIGURATION = {
-        #"region": None,  # taking his time
+        "region": None,  # taking his time
         "password_policy": {
             "minimum_length": 8,
             "require_uppercase": True,
