5bb730f56616ef62ef25bebef52669ee77e713ed
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2022-03-04 13:06:37 +0200

    Change the storage formant os .syndicate in S3 + some refactoring

--- syndicate/core/project_state/sync_processor.py
+++ syndicate/core/project_state/sync_processor.py
@@ -13,10 +13,10 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-import yaml
 
 from syndicate.commons.log_helper import get_logger, get_user_logger
 from syndicate.core.project_state.project_state import PROJECT_STATE_FILE
+from syndicate.core.project_state.project_state import ProjectState
 
 _LOG = get_logger('sync-processor')
 USER_LOG = get_user_logger()
@@ -29,17 +29,12 @@ def sync_project_state():
     if not s3.is_file_exists(bucket_name=bucket_name,
                              key=PROJECT_STATE_FILE):
         _LOG.debug('Remote .syndicate file does not exists. Pushing...')
-        s3.put_object(file_obj=yaml.dump(PROJECT_STATE),
-                      key=PROJECT_STATE_FILE,
-                      bucket=bucket_name,
-                      content_type='application/x-yaml')
+        PROJECT_STATE.save_to_remote()
         _LOG.debug('Push successful')
 
     else:
         _LOG.debug('Syncing with remote .syndicate file.')
-        remote_project_state = s3.load_file_body(bucket_name=bucket_name,
-                                                 key=PROJECT_STATE_FILE)
-        remote_project_state = yaml.unsafe_load(remote_project_state)
+        remote_project_state = ProjectState.get_remote()
 
         _LOG.debug('Actualizing the project state...')
         PROJECT_STATE.actualize_locks(remote_project_state)
@@ -50,10 +45,7 @@ def sync_project_state():
         PROJECT_STATE.save()
 
         _LOG.debug('Synced the .syndicate file. Pushing...')
-        s3.put_object(file_obj=yaml.dump(PROJECT_STATE),
-                      key=PROJECT_STATE_FILE,
-                      bucket=bucket_name,
-                      content_type='application/x-yaml')
+        PROJECT_STATE.save_to_remote()
         _LOG.debug('Push successful')
         _LOG.debug('Remote .syndicate file pulled')
     _LOG.info('Project state file has been successfully synced')
