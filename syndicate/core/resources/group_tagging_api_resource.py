429e2d5d5ad97db1806601013181812baaf791c3
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2022-11-23 11:48:22 +0200

    Probably fix taggin in case the number of resources is more than 20

--- syndicate/core/resources/group_tagging_api_resource.py
+++ syndicate/core/resources/group_tagging_api_resource.py
@@ -16,6 +16,8 @@
 from syndicate.commons.log_helper import get_logger, get_user_logger
 from syndicate.connection import ResourceGroupsTaggingAPIConnection
 from syndicate.core.constants import LAMBDA_TYPE
+from syndicate.core.resources.helper import chunks
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 _LOG = get_logger('syndicate.core.resources.group_tagging_api_resource')
 USER_LOG = get_user_logger()
@@ -56,25 +58,42 @@ class TagsApiResource:
             USER_LOG.info('No tags are specified in config. Skipping...')
             return
         arns = self._extract_arns(output)
-        failed_resources = self.connection.tag_resources(resources_arns=arns,
-                                                         tags=self.tags)
-        if not failed_resources:
+        failed_arns = []
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.connection.tag_resources, batch,
+                                self.tags) for batch in chunks(arns, 20)
+            ]
+            for future in as_completed(futures):
+                failed = future.result()
+                if failed:
+                    failed_arns.extend(failed.keys())
+        if not failed_arns:
             USER_LOG.info(f'Tags: {self.tags} were successfully applied to '
                           f'all resources')
         else:
             USER_LOG.warn(f'Couldn\'t apply tags for resources: '
-                          f'{failed_resources.keys()}')
+                          f'{failed_arns}')
 
     def remove_tags(self, output: dict):
         if not self.tags:
             USER_LOG.info('No tags are specified in config. Skipping...')
             return
         arns = self._extract_arns(output)
-        failed_resources = self.connection.untag_resources(
-            resources_arns=arns, tag_keys=list(self.tags.keys()))
-        if not failed_resources:
+        failed_arns = []
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    self.connection.untag_resources, batch,
+                    list(self.tags.keys())) for batch in chunks(arns, 20)
+            ]
+            for future in as_completed(futures):
+                failed = future.result()
+                if failed:
+                    failed_arns.extend(failed.keys())
+        if not failed_arns:
             USER_LOG.info(f'Tags {list(self.tags.keys())} were successfully '
                           f'removed from all resources')
         else:
             USER_LOG.warn(f'Couldn\'t remove tags from resources: '
-                          f'{failed_resources.keys()}')
+                          f'{failed_arns}')
