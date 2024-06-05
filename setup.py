e5ecd1bf1163fc3e433246e1236ba143cb0944c1
Mykhailo <mykhailo_kutsybala@epam.com>
2024-06-05 17:34:19 +0300

    version raised

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-syndicate',
-    version='1.11.7',
+    version='1.12.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
