1fcefd122dfd9553d1495eb9e2745e3c4b2460f7
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-06-18 21:51:17 +0400

    FunctionResponseType: fixed json serialization

--- plugin/deployment-configuration-annotations/src/main/java/com/syndicate/deployment/annotations/events/FunctionResponseType.java
+++ plugin/deployment-configuration-annotations/src/main/java/com/syndicate/deployment/annotations/events/FunctionResponseType.java
@@ -1,16 +1,32 @@
 package com.syndicate.deployment.annotations.events;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Map;
+import java.util.Optional;
+
 public enum FunctionResponseType {
+
     REPORT_BATCH_ITEM_FAILURES("ReportBatchItemFailures");
 
-    private final String stringValue;
+    private final String jsonValue;
+    private final static Map<String, FunctionResponseType> map = Map.of(
+            "ReportBatchItemFailures",
+            REPORT_BATCH_ITEM_FAILURES
+    );
 
-    FunctionResponseType(String stringValue) {
-        this.stringValue = stringValue;
+    FunctionResponseType(String jsonValue) {
+        this.jsonValue = jsonValue;
     }
 
-    @Override
-    public String toString() {
-        return stringValue;
+    @JsonCreator
+    public static FunctionResponseType fromValue(String value) {
+        return Optional.ofNullable(map.get(value)).orElseThrow();
+    }
+
+    @JsonValue
+    public String getJsonValue() {
+        return jsonValue;
     }
 }
