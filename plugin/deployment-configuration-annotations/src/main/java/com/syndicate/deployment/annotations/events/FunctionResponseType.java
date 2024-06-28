401856f2323fc2dc7eb1ca6d80c4e60ae15f9ac6
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-06-28 14:34:15 +0400

    FunctionResponseType: removed @JsonCreator and Map

--- plugin/deployment-configuration-annotations/src/main/java/com/syndicate/deployment/annotations/events/FunctionResponseType.java
+++ plugin/deployment-configuration-annotations/src/main/java/com/syndicate/deployment/annotations/events/FunctionResponseType.java
@@ -1,28 +1,15 @@
 package com.syndicate.deployment.annotations.events;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-
-import java.util.Map;
-import java.util.Optional;
 
 public enum FunctionResponseType {
 
     REPORT_BATCH_ITEM_FAILURES("ReportBatchItemFailures");
 
     private final String jsonValue;
-    private final static Map<String, FunctionResponseType> map = Map.of(
-            "ReportBatchItemFailures",
-            REPORT_BATCH_ITEM_FAILURES
-    );
 
     FunctionResponseType(String jsonValue) {
         this.jsonValue = jsonValue;
-    }
-
-    @JsonCreator
-    public static FunctionResponseType fromValue(String value) {
-        return Optional.ofNullable(map.get(value)).orElseThrow();
     }
 
     @JsonValue
