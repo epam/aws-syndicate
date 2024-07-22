44cf43f8a38a596b3efbd7d8fbd1d61869dc1bf6
Roman_Ivanov2 <roman_ivanov2@epam.com>
2024-07-22 10:21:13 +0300

    add Java example for API Gateway + Cognito

---
+++ examples/java/demo-apigateway-cognito/jsrc/main/java/com/demoapigatewaycognito/handler/PostSignUpHandler.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024 EPAM Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.demoapigatewaycognito.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.demoapigatewaycognito.dto.SignUp;
+import org.json.JSONObject;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
+
+/**
+ * Created by Roman Ivanov on 7/20/2024.
+ */
+public class PostSignUpHandler extends CognitoSupport implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public PostSignUpHandler(CognitoIdentityProviderClient cognitoClient) {
+        super(cognitoClient);
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        try {
+            SignUp signUp = SignUp.fromJson(requestEvent.getBody());
+
+            // sign up
+            String userId = cognitoSignUp(signUp)
+                    .user().attributes().stream()
+                    .filter(attr -> attr.name().equals("sub"))
+                    .map(AttributeType::value)
+                    .findAny()
+                    .orElseThrow(() -> new RuntimeException("Sub not found."));
+            // confirm sign up
+            String idToken = confirmSignUp(signUp)
+                    .authenticationResult()
+                    .idToken();
+
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withBody(new JSONObject()
+                            .put("message", "User has been successfully signed up.")
+                            .put("userId", userId)
+                            .put("accessToken", idToken)
+                            .toString());
+        } catch (Exception e) {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(400)
+                    .withBody(new JSONObject().put("error", e.getMessage()).toString());
+        }
+    }
+
+}
