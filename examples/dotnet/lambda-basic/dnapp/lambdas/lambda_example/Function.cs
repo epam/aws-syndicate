67b84201149645f4bfb754c0ea39ec91250c9aaa
Mykhailo Kutsybala <mykhailo_kutsybala@epam.com>
2024-10-08 17:30:27 +0300

    Added an example of a lambda function with runtime dotnet

---
+++ examples/dotnet/lambda-basic/dnapp/lambdas/lambda_example/Function.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambdaFunction;
+
+public class Function
+{
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = "Hello world!",
+            Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+        };
+    }
+}
