b26e402e0a756a5bb0c2263b6f34c0da3b5ecbb6
Roman_Ivanov2 <roman_ivanov2@epam.com>
2025-05-20 17:43:31 +0300

    add a set of new Value Transformers related to RDS

--- plugin/deployment-configuration-annotations/src/main/java/com/syndicate/deployment/model/environment/ValueTransformer.java
+++ plugin/deployment-configuration-annotations/src/main/java/com/syndicate/deployment/model/environment/ValueTransformer.java
@@ -3,8 +3,13 @@ package com.syndicate.deployment.model.environment;
 public enum ValueTransformer {
 
     NONE(null, null, null),
+
     USER_POOL_NAME_TO_USER_POOL_ID( "resource_name", "cognito_idp", "id"),
-    USER_POOL_NAME_TO_CLIENT_ID( "resource_name", "cognito_idp", "client_id");
+    USER_POOL_NAME_TO_CLIENT_ID( "resource_name", "cognito_idp", "client_id"),
+
+    RDS_DB_CLUSTER_NAME_TO_ENDPOINT( "resource_name", "rds_db_cluster", "endpoint"),
+    RDS_DB_CLUSTER_NAME_TO_READER_ENDPOINT( "resource_name", "rds_db_cluster", "reader_endpoint"),
+    RDS_DB_CLUSTER_NAME_TO_MASTER_USER_SECRET_NAME( "resource_name", "rds_db_cluster", "master_user_secret_name");
 
     private final String sourceParameter;
     private final String resourceType;
